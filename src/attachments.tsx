3859c639d17f8b14aac0213e324e8af95ce6f3f2
Nebulis <nebounet@gmail.com>
2020-04-08 18:41:12 +0800

    lol

--- src/attachments.tsx
+++ src/attachments.tsx
@@ -1,42 +1,12 @@
-import React, { FunctionComponent, useState } from "react";
-import { Attachment, Renderer } from "@govtechsg/decentralized-renderer-react-components";
-import { get, times } from "lodash";
-import { Document, Page, pdfjs } from "react-pdf";
+import React from "react";
+import { Attachment, PdfRenderer, UnsupportedRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { get } from "lodash";
 import QRCode from "qrcode.react";
 
-// TODO check this https://github.com/wojtekmaj/react-pdf#browserify-and-others
-// TODO imported from "@govtechsg/decentralized-renderer-react-components" => need to export from the lib ? be careful on the size on the bundle
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-const PdfRenderer: FunctionComponent<Renderer> = ({ attachment }) => {
-  const [numberOfPages, setNumberOfPages] = useState(0);
-
-  return (
-    <Document
-      file={`data:application/pdf;base64,${attachment.data}`}
-      onLoadSuccess={({ numPages }) => {
-        setNumberOfPages(numPages);
-      }}
-    >
-      <style
-        scoped
-        dangerouslySetInnerHTML={{
-          __html: `
-      canvas {
-        margin: auto;
-      }
-    `
-        }}
-      />
-      {times(numberOfPages, index => (
-        // TODO: Dynamically resize width to fit container
-        // https://github.com/wojtekmaj/react-pdf/issues/129
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
-  );
-};
-
-export function fullAttachmentRenderer<D extends Document>(attachment: Attachment): React.FunctionComponent {
+export function fullAttachmentRenderer<D extends Document>(
+  attachment: Attachment,
+  document: Document
+): React.FunctionComponent {
   if (attachment.type === "application/pdf") {
     // eslint-disable-next-line react/display-name
     return () => (
@@ -57,10 +27,5 @@ export function fullAttachmentRenderer<D extends Document>(attachment: Attachmen
 
   // TODO would be better to not have to handle this case, improvement to be done in the lib
   // eslint-disable-next-line react/display-name
-  return () => (
-    <div>
-      <h2>Rendering of this type of attachment is not supported.</h2>
-      <p>Please check your mime-type: {attachment.type}</p>
-    </div>
-  );
+  return () => <UnsupportedRenderer attachment={attachment} />;
 }
